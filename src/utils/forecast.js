212b3aee6585b7bbc946c707f5458971378b795d
aniekan <aniekanbon@gmail.com>
2020-07-24 20:44:59 +0100

    Remove request and instal axios library

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,28 +1,29 @@
-const request = require("request");
+const axios = require("axios");
 
 const forecast = (latArg, longArg, callback) => {
-  const url =
+  const url = 
     "http://api.openweathermap.org/data/2.5/onecall?lat=" +
-    latArg +
-    "&lon=" +
-    longArg +
-    "&units=metric&appid=a2e20ad903fd6b1ddb5670bb56c2151e";
+     latArg + "&lon=" + longArg + "&units=metric&appid=a2e20ad903fd6b1ddb5670bb56c2151e";
 
-  request({ url, json: true }, (error, { body }) => {
-    if (error) {
-      callback("Unable to connect to weather service", undefined);
-    } else if (body.message) {
-      callback("Unable to find location", undefined);
-    } else {
-      callback(
-        undefined, {
-          dailyForecast: body.daily[0].weather[0].description,
-          temp: "It is currently " + body.current.temp + " degrees celsius out there. There is going to be " + body.current.weather[0].description +"." 
+     axios({method: "get", url})
+      .then(res => {
+        if (res.data.message) {
+          return callback("Unable to find location", undefined);
+
         }
-       
-      );
-    }
-  });
-};
+
+        callback(undefined, {
+          dailyForecast: res.data.daily[0].weather[0].description,
+          temp: "It is currently " + res.data.current.temp + " degrees celsius out there. There is going to be " + res.data.current.weather[0].description +"." 
+        });
+
+      })
+      .catch(error => {
+        
+        callback("Unable to connect to weather services", undefined);
+      })
+}
+
+
 
 module.exports = forecast;
--- src/utils/geocode.js
+++ src/utils/geocode.js
@@ -1,24 +1,27 @@
-const request = require("request");
+const axios = require("axios");
 
 const geocode = (address, callback) => {
-  const url =
-    "https://api.mapbox.com/geocoding/v5/mapbox.places/" +
-    address +
-    ".json?access_token=pk.eyJ1IjoiZWthbjA3IiwiYSI6ImNrYmtwdXE4aDExajYydXBqbWFpbnpkYWIifQ.-ghV7u1OW10kpdfHahBiHA&limit=1";
+  const url = 
+  "https://api.mapbox.com/geocoding/v5/mapbox.places/" +
+  address +
+  ".json?access_token=pk.eyJ1IjoiZWthbjA3IiwiYSI6ImNrYmtwdXE4aDExajYydXBqbWFpbnpkYWIifQ.-ghV7u1OW10kpdfHahBiHA&limit=1";
 
-  request({ url, json: true }, (error, { body }) => {
-    if (error) {
-      callback("Unable to connect to location services!", undefined);
-    } else if (body.features.length === 0) {
-      callback("Unable to find location. Try another search.", undefined);
-    } else {
+  axios({method: "get", url})
+    .then(res => {
+      if (res.data.features.length === 0) {
+        return callback("Unable to find location. Try another search.", undefined);
+        
+      }
       callback(undefined, {
-        latitude: body.features[0].center[1],
-        longitude: body.features[0].center[0],
-        location: body.features[0].place_name,
+        latitude: res.data.features[0].center[1],
+        longitude: res.data.features[0].center[0],
+        location: res.data.features[0].place_name,
       });
-    }
-  });
+    })
+    .catch(error => {
+      callback("Unable to connect to location services!", undefined);
+      
+    })
 };
 
 module.exports = geocode;
